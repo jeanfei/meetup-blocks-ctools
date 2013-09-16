2c02e08386f2aa4aea0fdb36ad0425567affd837
Jean-Fei Taing <jeanfei.taing@gmail.com>
2013-09-16 23:43:51 +0200

    Update .gitignore after rename folder + add common site install sh to make install easier

--- site-install.sh
+++ site-install.sh
@@ -1,11 +1,48 @@
 #!/bin/sh
 
-echo "Start - Installing meetup blocks demo"
-cd blocks; drush site-install meetup_blocks_profile --account-pass='admin' --site-name='Meetup - Blocks demo' -yv
-echo "End - Installing meetup blocks demo"
-cd ..
+# Installing meetup_blocks_profile profile
+meetup_blocks_profile() {
+  echo "Start - Installing meetup blocks demo"
+  cd drupal7-demo; drush site-install meetup_blocks_profile --account-pass='admin' --site-name='Meetup - Blocks demo' --locale='fr' -y
+  echo "End - Installing meetup blocks demo"
+}
 
-echo "-----------------------------"
-echo "Start - Installing meetup ctools demo"
-cd ctools; drush site-install meetup_ctools_profile --account-pass='admin' --site-name='Meetup - Ctools demo' -yv
-echo "End - Installing meetup ctools demo"
+# Installing meetup_context_profile profile
+meetup_context_profile() {
+  echo "Start - Installing meetup context demo"
+  cd drupal7-demo; drush site-install meetup_context_profile --account-pass='admin' --site-name='Meetup - Context demo' --locale='fr' -y
+  echo "End - Installing meetup context demo"
+}
+
+# Installing meetup_ctools_profile profile
+meetup_ctools_profile() {
+  echo "Start - Installing meetup ctools demo"
+  cd drupal7-demo; drush site-install meetup_ctools_profile --account-pass='admin' --site-name='Meetup - Ctools demo' --locale='fr' -y
+  echo "End - Installing meetup ctools demo"
+}
+
+
+# Prompt for user
+while true; do
+  echo
+  echo "Installation profile"
+  echo "1. Installing Drupal7 meetup blocks demo"
+  echo "2. Installing Drupal7 meetup context demo"
+  echo "3. Installing Drupal7 meetup ctools demo"
+  # echo "4. Installing Drupal8 meetup layout demo"
+  echo
+
+  if [ -z $1 ]
+  then
+    read -p "Your selection : " selection
+  else
+    selection=$1
+  fi
+
+  case $selection in
+      1 ) meetup_blocks_profile; break;;
+      2 ) meetup_context_profile; break;;
+      3 ) meetup_ctools_profile; break;;
+      * ) echo "Please select a valid choice.";;
+    esac
+done
